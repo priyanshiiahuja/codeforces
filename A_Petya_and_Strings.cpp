269c1ac2d0471cd532bde773e53a960ee83947c7
Priyanshi Ahuja <priyanshiahuja2020@gmail.com>
2024-09-03 03:48:16 +0530

    modified

--- A_Petya_and_Strings.cpp
+++ A_Petya_and_Strings.cpp
@@ -5,16 +5,16 @@ int main()
 {
     string s, s1;
     cin >> s >> s1;
-    transform(s.begin(), s.end(), s.begin(), :: tolower);
-    transform(s1.begin(), s1.end(), s1.begin(), :: tolower);
-    int result = s.compare(s1);
-    if (result < 0) cout << "-1";
-    else if (result > 0) cout << "1";
-    else cout << "0"; 
-    // for (int i = 0; i <= s1.size(); i++){
-    //     if (s[i] == s1[i]) return 0;
-    //     else if (s[i] > s1[i]) return 1;
-    //     else return -1;
-    // }
+    for (int i = 0; i < s1.size(); i++){
+        if (s1[i] >= 65 && s1[i] <= 90){
+            s1[i] = s1[i] + 32;
+        }
+        if (s[i] >= 65 && s[i] <= 90){
+            s[i] = s[i] + 32;
+        }
+    }
+    if (s == s1) cout << "0";
+    else if (s > s1) cout << "1";
+    else cout << "-1" ;
     return 0;
 }
